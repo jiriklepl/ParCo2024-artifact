6ad71ff48f7234508757717533824086cbd6c462
jiriklepl <jiriklepl@seznam.cz>
2023-12-27 11:21:33 +0100

    make the plots more a little bit smaller

--- generate_plots.sh
+++ generate_plots.sh
@@ -5,7 +5,7 @@
 ./parse_data.sh PolybenchC-Noarr-tuned/extralarge-data > polybench-c-tuned.csv
 ./parse_data.sh PolyBenchGPU-Noarr/data > polybench-gpu.csv
 
-./plot-tbb.R polybench-c-tbb.csv 1.3 3 TBB
-./plot-others.R polybench-gpu.csv 1.3 3 GPU
-./plot-polybench.R polybench-c.csv 4 3
-./plot-others.R polybench-c-tuned.csv 1.3 3 serial
+./plot-tbb.R polybench-c-tbb.csv 1.3 2.5 TBB
+./plot-others.R polybench-gpu.csv 1.3 2.5 GPU
+./plot-polybench.R polybench-c.csv 4 2.5
+./plot-others.R polybench-c-tuned.csv 1.3 2.5 serial
