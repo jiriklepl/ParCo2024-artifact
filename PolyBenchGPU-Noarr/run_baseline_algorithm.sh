90951235dd0f2fa3b484251e65d9e91f10bbb893
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2024-04-14 05:18:19 +0200

    support NUM_RUNS

--- PolyBenchGPU-Noarr/run_baseline_algorithm.sh
+++ PolyBenchGPU-Noarr/run_baseline_algorithm.sh
@@ -3,10 +3,12 @@
 prefix="$1"
 file="$2"
 
+NUM_RUNS=${NUM_RUNS:-10}
+
 printf "\t$prefix: "
 "$file" 2>/dev/null | grep -oE "[0-9]+\.[0-9]{2,}"
 
-for _ in $(seq 10); do
+for _ in $(seq "$NUM_RUNS"); do
     printf "\t$prefix: "
     "$file" 2>/dev/null | grep -oE "[0-9]+\.[0-9]{2,}"
 done
--- PolyBenchGPU-Noarr/run_noarr_algorithm.sh
+++ PolyBenchGPU-Noarr/run_noarr_algorithm.sh
@@ -3,10 +3,12 @@
 prefix="$1"
 algorithm="$2"
 
+NUM_RUNS=${NUM_RUNS:-10}
+
 printf "\t$prefix: "
 build/runner "$algorithm" 2>&1 1>/dev/null | grep -oE "[0-9]+\.[0-9]{2,}"
 
-for _ in $(seq 10); do
+for _ in $(seq "$NUM_RUNS"); do
     printf "\t$prefix: "
     build/runner "$algorithm" 2>&1 1>/dev/null | grep -oE "[0-9]+\.[0-9]{2,}"
 done
